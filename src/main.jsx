49760350cfb2587cb2de881c51364c8c4d69105a
Jayden <oracoke@gmail.com>
2024-06-21 18:41:19 +0800

    delete file

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,6 @@
-import Popup from '@/popup'
-import { ConfigProvider } from 'antd'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import Popup from "@/popup";
+import React from "react";
+import ReactDOM from "react-dom/client";
 // 在popup页面调试content script，仅用于开发环境，build前记得要注释掉。
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <ConfigProvider >
-        <Popup />
-    </ConfigProvider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<Popup />);
